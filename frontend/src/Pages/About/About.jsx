3c87e18c645a9d742a8db3ab468ef8206814f341
Anjali Perera <85072134+AnjaliPerera@users.noreply.github.com>
2024-10-21 19:51:02 +0000

    dinuka modified home

--- frontend/src/Pages/About/About.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const About = () => {
-  return (
-    <div className=" px-[30px]  pt-[232px] pb-[47px] w-full h-screen overflow-y-scroll bg-[#FFF3E0] md:px-[100px]">About</div>
-  )
-}
-
-export default About
--- frontend/src/Pages/Custom/Custom.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const Custom = () => {
-  return (
-    <div className="px-[30px]  pt-[232px] pb-[47px] w-full h-screen overflow-y-scroll bg-[#FFF3E0] md:px-[100px]">Custom</div>
-  )
-}
-
-export default Custom
