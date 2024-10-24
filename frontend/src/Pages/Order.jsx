1bc566a7debf23a62abcdbd47c7c672a6dfe43df
Vidura12 <nayanodyav@gmail.com>
2024-10-24 20:58:45 +0530

    show welcome image design

--- frontend/src/Pages/Order.jsx
+++ frontend/src/Pages/Order.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import image1 from '../assets/Rectangle 8.png'
 
 export default function Order() {
   return (
-    <div className="text-3xl font-bold underline">
-      Order
+    <div className="lg:container flex flex-col items-center justify-center text-center relative">
+      <div className="text-center">
+        <img
+          src={image1}
+          alt=""
+          style={{
+            width: "1040px",
+            height: "600px",
+            backgroundColor: "#FFF3E0",
+          }}
+        />
+        <h1
+          className="absolute top-48 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white tracking-wide text-8xl font-bold font-mono"
+          style={{
+            textShadow: `
+              -1px -1px 0px black,
+              1px -1px 0px black,
+              -1px 1px 0px black,
+              1px 1px 0px black
+            `,
+          }}
+        >
+          Welcome
+        </h1>
+        <button
+          className="absolute top-96 left-1/4 transform -translate-x-1/2 bg-blue-500 text-white py-4 px-14 font-bold rounded-full"
+          style={{ backgroundColor: "#D84315" }}
+        >
+          Order
+        </button>
+      </div>
+        
+      
     </div>
   )
 }
